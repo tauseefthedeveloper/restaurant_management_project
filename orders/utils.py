cbf3a0aca9126e817d5542afda82b98c8fa78fe5
Mr. Tauseef Ahmad <145428264+tauseefthedeveloper@users.noreply.github.com>
2025-09-18 20:01:36 +0530

    47822

--- orders/utils.py
+++ orders/utils.py
@@ -3,13 +3,15 @@ import string
 from orders.models import Coupon
 
 
-def generate_coupon_code(length=10):
+def generate_coupon_code(length=10,max_attempts=1000):
     
     #Generate a coupon code that is unique
     alphabet = string.ascii_uppercase + string.digits # A to Z and 0 to 9
-    while True:
+    for attemts in range(max_attempts):
         #Generate a unique code
-        code = ''.join(secrets.choice(alphabet) for _ in range(length)) #e.g. XYZ1B2E65F
+        coupon_code = ''.join(secrets.choice(alphabet) for _ in range(length)) #e.g. XYZ1B2E65F
         #Check exits in DB or not
-        if not Coupon.objects.filter(code=code).exists():
-            return code
+        if not Coupon.objects.filter(code=coupon_code).exists():
+            return coupon_code
+    
+    raise RuntimeError("Failed to generate a unique code.")
