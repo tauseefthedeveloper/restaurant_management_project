a59208ed7b1b8ad3783e2e560efb32f601e63d8f
Mr. Tauseef Ahmad <145428264+tauseefthedeveloper@users.noreply.github.com>
2025-09-18 19:53:46 +0530

    47822

--- orders/utils.py
+++ orders/utils.py
@@ -3,7 +3,6 @@ import string
 from orders.models import Coupon
 
 
-
 def generate_coupon_code(length=10):
     
     #Generate a coupon code that is unique
@@ -12,5 +11,5 @@ def generate_coupon_code(length=10):
         #Generate a unique code
         code = ''.join(secrets.choice(alphabet) for _ in range(length)) #e.g. XYZ1B2E65F
         #Check exits in DB or not
-        if not Coupon.objects.filter(code=code).exits():
+        if not Coupon.objects.filter(code=code).exists():
             return code
