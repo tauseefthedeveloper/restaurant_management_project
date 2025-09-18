09805263636b8afa2c1f7deaa6b98850dd9aec4b
Mr. Tauseef Ahmad <145428264+tauseefthedeveloper@users.noreply.github.com>
2025-09-18 20:11:29 +0530

    47821

--- orders/models.py
+++ orders/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50, unique= True)
+    class Meta:
+        verbose_name = "Order Status"
+        verbose_name_plural = "Order Statuses"
+
+    def __str__(self):
+        return self.name
