47a28cfb3847286680c122133571241914b58e21
Mr. Tauseef Ahmad <145428264+tauseefthedeveloper@users.noreply.github.com>
2025-09-18 19:49:30 +0530

    47822

--- orders/utils.py
+++ orders/utils.py
@@ -2,13 +2,15 @@ import secrets
 import string
 from orders.models import Coupon
 
+
+
 def generate_coupon_code(length=10):
     
     #Generate a coupon code that is unique
-    alphabet=string.ascii_uppercase + string.digits
+    alphabet = string.ascii_uppercase + string.digits # A to Z and 0 to 9
     while True:
         #Generate a unique code
-        code=''.join(secrets.choice(alphabet) for _ in range(length))
+        code = ''.join(secrets.choice(alphabet) for _ in range(length)) #e.g. XYZ1B2E65F
         #Check exits in DB or not
         if not Coupon.objects.filter(code=code).exits():
             return code
