82b0c3750731729827a9895e75fe4d796f6b3001
Mr. Tauseef Ahmad <145428264+tauseefthedeveloper@users.noreply.github.com>
2025-09-22 16:52:35 +0530

    47820

--- orders/models.py
+++ orders/models.py
@@ -9,3 +9,12 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DeciamlField(max_digits=10, decimal_places=2)
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
